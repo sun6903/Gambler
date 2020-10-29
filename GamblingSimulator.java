d99fd6dcc5339ea3f94ed2a5958baea3aaa24798
sun6903 <62666209+sun6903@users.noreply.github.com>
2020-10-29 18:05:29 +0530

    Add files via upload

---
+++ GamblingSimulator.java
@@ -0,0 +1,15 @@
+public class GamblingSimulator
+{
+ int Stake = 100;
+ int Bet = 1;
+ int win = 1;
+ public static void main(String args[])
+{
+ int check = (int)Math.floor(Math.random()*10) % 2 
+ if (check==win){
+     System.out.println;
+ else
+    ;
+ }
+    
+}
