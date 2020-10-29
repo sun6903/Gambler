b6478d31ee57f906fb682117c06153ac4981ec75
sun6903 <62666209+sun6903@users.noreply.github.com>
2020-10-29 18:08:40 +0530

    Update GamblingSimulator.java

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -1,15 +1,37 @@
-public class GamblingSimulator
+public class Gamble
 {
- int Stake = 100;
- int Bet = 1;
- int win = 1;
- public static void main(String args[])
+	public static final int stake  = 100;
+	public static final int bets = 1;
+	public static final int Tday = 20;
+
+public static void main(String args[])
 {
- int check = (int)Math.floor(Math.random()*10) % 2 
- if (check==win){
-     System.out.println;
- else
-    ;
- }
-    
+System.out.println("Welcome to Gamble");
+
+int percent;
+int totalamount;
+	private static int Winlose(int stake) {
+	 if (Math.random() < 0.5) 
+	      return stake+1; // win $1
+	 else 
+	      return stake-1;    // lose $1
+	}
+   private static void Percent(double percent )
+   {
+	
+		if(stake < (0.5)*goal){
+			System.out.println("Continue the game");
+		else
+			System.out.println("Leave the game");
+		}
+   }
+   public static void main(String args[])
+   {
+		totalamount=stake+bets;
+   if (day=0; day<20; day++)
+		System.out.println("Total Amount for 20days:" + totalamount);
+  else
+		System.out.println("Loss");
+	}
+}
 }
